8d2044d1ba9a202f0c3bd1f62a1f78881bc5e101
Terrence Kgetedi <terrencekgetedi@gmail.com>
2025-09-11 19:50:42 +0200

    testing

--- src/components/Admin.jsx
+++ src/components/Admin.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Admin = () => {
   return (
     <div style={{ padding: "20px", textAlign: "center" }}>
-      <h2>Admin Panel</h2>
+      <h2>Admin Panel.</h2>
       <p>Welcome to the Water Pollution Project Admin Dashboard.</p>
     </div>
   );
